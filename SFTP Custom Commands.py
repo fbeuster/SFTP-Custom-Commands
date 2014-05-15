bc540ffd9b70dce2dddb0ae55949c9dad75bd850
fbeuster <felix.beuster@beusterse.de>
2014-05-15 14:11:20 +0200

    sync_down_windows; key bindings

--- SFTP Custom Commands.py
+++ SFTP Custom Commands.py
@@ -17,8 +17,31 @@ class SyncDownTabsCommand(sublime_plugin.WindowCommand):
 			win.focus_view(v)
 
 			# only sync sftp files
-			if re.match('.*sublime-sftp-browse.*', v.file_name()):
-				win.run_command('sftp_sync_down')
+			name = v.file_name()
+			if name != None:
+				if re.match('.*sublime-sftp-browse.*', name):
+					win.run_command('sftp_sync_down')
 
 		# select init view
 		win.focus_view(vc)
+
+class SyncDownWindowsCommand(sublime_plugin.WindowCommand):
+	def run(self):
+
+		# current window and view
+		cWin = self.window
+		cView = cWin.active_view()
+
+		for win in sublime.windows():
+			for v in win.views():
+				# switch view
+				win.focus_view(v)
+
+				# only sync sftp files
+				name = v.file_name()
+				if name != None:
+					if re.match('.*sublime-sftp-browse.*', name):
+						win.run_command('sftp_sync_down')
+
+		# select init view
+		cWin.focus_view(cView)
